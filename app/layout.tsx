ff677af88f560094e8c68c806c05b89f1344b459
Your Name <your-email@example.com>
2025-02-16 19:05:42 +0100

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="sq">
+      <head>
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
